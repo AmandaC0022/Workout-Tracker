36bdf689c8922dfe5fffd83c5fd58c5bdfd081b8
Amanda Morgan <AmandaC0022@gmail.com>
2021-10-22 11:29:52 -0500

    successfully seeded data to database!

--- server.js
+++ server.js
@@ -1,29 +1,60 @@
 const express = require("express");
 const logger = require("morgan");
-const mongojs = require("mongojs");
-
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3000;
 const app = express();
 const path = require('path'); 
-const router = express.Router(); 
-
-const databaseUrl = "workout";
-const collections = ["excerise"];
-const db = mongojs(databaseUrl, collections);
+// const router = express.Router(); 
+const db = require('./models'); 
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+app.set("views", path.join(__dirname, "views")); 
+
+// app.use('/', router); 
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+
+mongoose.connection.once('open', function() {
+  console.log('Connection has been made!'); 
+})
+.on('error', function(error) {
+  console.log("Connection Error:", error); 
+}); 
+
+app.get("/", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// this renders the exercise page
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/exercise.html'));
+});
+
+//this renders the stats page 
+// app.get("/stats", (req, res) => {
+//   res.sendFile(path.join(__dirname + '/public/stats.html'));
+// });
+
+// app.post("/exercise", ({ body }, res) => {
+//   Exercise.create(body)
+//     .then(dbexercise => {
+//       res.json(dbexercise);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
-});
-
-// this GET route works!
-app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/exercise.html'));
 });
