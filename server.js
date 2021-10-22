3e7cc386920794648d646f05fa10799351f83e30
Amanda Morgan <AmandaC0022@gmail.com>
2021-10-22 08:52:47 -0500

    I am able to render the exercise page now!

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const logger = require("morgan");
+const mongojs = require("mongojs");
+
 const mongoose = require("mongoose");
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
+const path = require('path'); 
+const router = express.Router(); 
+
+const databaseUrl = "workout";
+const collections = ["excerise"];
+const db = mongojs(databaseUrl, collections);
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
+});
+
+// this GET route works!
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/exercise.html'));
 });
