aba01b80b0949a30c2e5a68884a0786f2bd271db
Amanda Morgan <AmandaC0022@gmail.com>
2021-10-24 09:39:09 -0500

    updated mongoose connection to accommodate heroku and atlas

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ app.use(express.json());
 app.use(express.static("public"));
 // app.use(routes); 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 mongoose.connection.once('open', function() {
   console.log('Connection has been made!'); 
